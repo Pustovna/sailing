030488863cf03c202ce7af6c61a87411290e0cea
Anastasiya <pustoe0@gmail.com>
2025-01-31 23:05:17 +0000

    mui datepicker removed

--- app/ui/components/Calendar.tsx
+++ app/ui/components/Calendar.tsx
@@ -1,17 +1,17 @@
-'use client';
-import * as React from 'react';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// 'use client';
+// import * as React from 'react';
+// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function Calendar() {
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs} >
-      <DemoContainer  components={['DatePicker']} sx={{padding: '0'}}>
-        <DatePicker label="C" sx={{width: '100%'}} />
-        <DatePicker label="По" sx={{width: '100%'}} />
-      </DemoContainer>
-    </LocalizationProvider>
-  );
-}
+// export default function Calendar() {
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs} >
+//       <DemoContainer  components={['DatePicker']} sx={{padding: '0'}}>
+//         <DatePicker label="C" sx={{width: '100%'}} />
+//         <DatePicker label="По" sx={{width: '100%'}} />
+//       </DemoContainer>
+//     </LocalizationProvider>
+//   );
+// }
